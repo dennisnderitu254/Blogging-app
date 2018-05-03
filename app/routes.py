4ebaecf651e3629b33e4b982888b308b08b453d7
Dennis Nderitu <dknderitu@gmail.com>
2018-05-03 11:25:51 +0300

    modified login field

--- app/routes.py
+++ app/routes.py
@@ -25,4 +25,4 @@ def login():
         flash('Login requested for user {}, remember_me={}'.format(
             form.username.data, form.remember_me.data))
         return redirect(url_for('index'))
-    return render_template('login.html', title='Sign In', form=form)
+    return render_template('login.html',  title='Sign In', form=form)
