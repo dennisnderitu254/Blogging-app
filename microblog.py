4c2565e6969564724a1e859be7c0c623972009ec
Dennis Nderitu <dknderitu@gmail.com>
2018-05-10 11:54:17 +0300

    enabling cli commands to work

--- microblog.py
+++ microblog.py
@@ -1,9 +1,8 @@
-
-from app import app, db
+from app import app, db, cli
 from app.models import User, Post
-
 
 
 @app.shell_context_processor
 def make_shell_context():
-	return{'db':db, 'User': User, 'Post': Post}
+return {'db': db, 'User': User, 'Post': Post}
+
